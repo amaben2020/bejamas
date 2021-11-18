d058bf49438744c2ac6eed19331de7f9abd35883
amaben2020 <amaben202phantom@gmail.com>
2021-11-18 18:13:58 +0100

    scaffolding the app, building functions, writing tests, building reusable components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { CartProvider } from '../context/CartState';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+  return (
+    <CartProvider>
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer position="top-center" />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </CartProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
